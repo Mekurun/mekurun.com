9932d9a54bf153facbb70e603651a67dda5ff713
Yuki Mihashi <yukimihashih@gmail.com>
2020-06-01 23:20:00 +0900

    Delete not used valiable from plugin

--- _plugins/jekyll_subcourse_permalinks.rb
+++ _plugins/jekyll_subcourse_permalinks.rb
@@ -6,7 +6,6 @@ module Jekyll
     def generate(site)
       site.posts.each do |item|
         @parent = item.data['parent']
-        @title = item.data['title']
         unless @parent.nil?
           item.data['permalink'] = "/:categories/#{@parent}/:title/"
         end
