f8eb64526ea0b231e4fb1415807458a752b30523
Yuki Mihashi <yukimihashih@gmail.com>
2020-06-08 22:21:00 +0900

    Change cloudinary path and conditional expression

--- js/usecloudinary.js
+++ js/usecloudinary.js
@@ -4,14 +4,14 @@ function setsrc() {
     var images = document.getElementsByTagName('img');
     for (var i = 0; i < images.length; i++) {
       if(images[i].dataset.src){
-        if('{{ site.url }}' == 'http://localhost:4000') {
+        if('{{ site.url }}' != 'https://mekurun.com') {
           images[i].src = images[i].dataset.src;
         }else{
           var display = images[i].style.display;
           if(display == "none") images[i].style.display = "block";
           var w = images[i].clientWidth;
           var h = images[i].clientHeight;
-          images[i].src = "https://res.cloudinary.com/yuki384/image/fetch/" + "w_" + w + ",h_" + h + ",c_fit,q_auto,f_auto/" + images[i].dataset.src;
+          images[i].src = "https://res.cloudinary.com/nztm/image/fetch/" + "w_" + w + ",h_" + h + ",c_fit,q_auto,f_auto/" + images[i].dataset.src;
           images[i].style.display = display;
         }
       }
