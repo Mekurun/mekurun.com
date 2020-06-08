4b0c555a0e5d353726b1d3f720698877f8d6347e
Yuki Mihashi <yukimihashih@gmail.com>
2020-06-08 22:40:19 +0900

    cloudinary retina 対応

--- js/usecloudinary.js
+++ js/usecloudinary.js
@@ -11,7 +11,7 @@ function setsrc() {
         if(display == "none") images[i].style.display = "block";
         var w = images[i].clientWidth;
         var h = images[i].clientHeight;
-        images[i].src = "https://res.cloudinary.com/nztm/image/fetch/" + "w_" + w + ",h_" + h + ",c_fit,q_auto,f_auto/" + images[i].dataset.src;
+        images[i].src = "https://res.cloudinary.com/nztm/image/fetch/" + "w_" + w + ",h_" + h + ",c_fit,q_auto,f_auto/dpr_"+ window.devicePixelRatio + ".0/" + images[i].dataset.src;
         images[i].style.display = display;
       }
     }
