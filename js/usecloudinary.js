5b827fe20a12f5a63cb215ecec8df976d778584b
Yuki Mihashi <31533303+yuki384@users.noreply.github.com>
2020-06-21 10:05:40 +0900

    本番環境は画像パス置き換えられているのでcloudinary設定を変更する

--- js/usecloudinary.js
+++ js/usecloudinary.js
@@ -10,9 +10,9 @@ function setsrc() {
         var display = images[i].style.display;
         if(display == "none") images[i].style.display = "block";
         if (images[i].dataset.width) {
-          images[i].src = "https://res.cloudinary.com/nztm/image/fetch/" + "w_" + images[i].dataset.width + ",c_fit,q_auto,f_auto/dpr_"+ window.devicePixelRatio + ".0/" + '{{ site.url }}' + images[i].dataset.src;
+          images[i].src = "https://res.cloudinary.com/nztm/image/fetch/" + "w_" + images[i].dataset.width + ",c_fit,q_auto,f_auto/dpr_"+ window.devicePixelRatio + ".0/" + images[i].dataset.src;
         }else {
-          images[i].src = "https://res.cloudinary.com/nztm/image/fetch/c_fit,q_auto,f_auto/" + '{{ site.url }}' + images[i].dataset.src;
+          images[i].src = "https://res.cloudinary.com/nztm/image/fetch/c_fit,q_auto,f_auto/" + images[i].dataset.src;
         }
         images[i].style.display = display;
       }
