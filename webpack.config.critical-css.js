465a56580fca9f60897d041658b28b52a3a1fe10
hota1024 <hotalog@hota1024.com>
2020-08-06 16:17:23 +0900

    🔧 CriticalCSSとPurgeCSSのWebpack設定ファイルを追加

---
+++ webpack.config.critical-css.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const glob = require("glob");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlCriticalWebpackPlugin = require("html-critical-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/js/top-main.js",
+  output: {
+    filename: "top-main.js",
+    path: path.resolve(__dirname, "_site/assets/js"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./_site/index.html"),
+      filename: "index.html",
+      inject: true,
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+    new PurgecssPlugin({
+      paths: glob.sync(`_site/**/*`, { nodir: true }),
+    }),
+    new HtmlCriticalWebpackPlugin({
+      base: path.join(path.resolve(__dirname), "_site/"),
+      src: "index.html",
+      dest: "index.html",
+      inline: true,
+      width: 1920,
+      height: 1080,
+      penthouse: {
+        blockJSRequests: false,
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sc|c|sa)ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
