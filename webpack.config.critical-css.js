b3a54ec8f1ead28284a49e8535755a63d30ff154
hota1024 <hotalog@hota1024.com>
2020-08-10 17:47:34 +0900

    🔧 CriticalCSSの設定を削除

--- webpack.config.critical-css.js
+++ webpack.config.critical-css.js
@@ -2,47 +2,43 @@ const path = require("path");
 const glob = require("glob");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const PurgecssPlugin = require("purgecss-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const HtmlCriticalWebpackPlugin = require("html-critical-webpack-plugin");
+
+const PATHS = {
+  src: path.join(__dirname, "_site"),
+};
 
 module.exports = {
-  mode: "production",
   entry: "./src/js/top-main.js",
   output: {
     filename: "top-main.js",
-    path: path.resolve(__dirname, "_site/assets/js"),
+    path: path.join(__dirname, "_site/assets/js"),
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./_site/index.html"),
-      filename: "index.html",
-      inject: true,
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
-    }),
-    new PurgecssPlugin({
-      paths: glob.sync(`_site/**/*`, { nodir: true }),
-    }),
-    new HtmlCriticalWebpackPlugin({
-      base: path.join(path.resolve(__dirname), "_site/"),
-      src: "index.html",
-      dest: "index.html",
-      inline: true,
-      width: 375,
-      height: 667,
-      penthouse: {
-        blockJSRequests: false,
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: "styles",
+          test: /\.css$/,
+          chunks: "all",
+          enforce: true,
+        },
       },
-    }),
-  ],
+    },
+  },
   module: {
     rules: [
       {
-        test: /\.(sc|c|sa)ss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new PurgecssPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
+    }),
+  ],
 };
